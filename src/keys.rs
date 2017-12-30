f6db5054f71bd954f85693d7c3d09f80d12a772e
kavya <soldatargent@gmail.com>
2017-12-30 11:24:35 +0530

    fixed get_key_type to return result and removed from trait impl

--- src/keys.rs
+++ src/keys.rs
@@ -9,29 +9,15 @@ pub enum Key {
     MISCFUNCTION,
     PF,
     PRIVATEKEYPAD,
-    INVALID,
 }
 
-impl From<u64> for Key{
-    fn from(int: u64) -> Key{
-        match int {
-            0 => Key::KEYPAD,
-            1 => Key::FUNCTION,
-            2 => Key::MISCFUNCTION,
-            3 => Key::PF,
-            4 => Key::PRIVATEKEYPAD,
-            _ => Key::INVALID,
-        }
-    }
-}
-
-pub fn get_key_type(ksym: KeySym) -> Key {
+pub fn get_key_type(ksym: KeySym) -> Result<Key, &'static str> {
     match () {
-        _ if ksym >= XK_space as u64 && ksym <= XK_equal as u64 => Key::KEYPAD,
-        _ if ksym >= XK_F1 as u64 && ksym <= XK_F35 as u64 => Key::FUNCTION,
-        _ if ksym >= 0x11000000 && ksym <= 0x1100FFFF => Key::PF,
-        _ if ksym >= XK_Select as u64 && ksym <= XK_Break as u64 => Key::MISCFUNCTION,
-        _ => Key::INVALID
-
+        _ if ksym >= XK_space as u64 && ksym <= XK_equal as u64 => Ok(Key::KEYPAD),
+        _ if ksym >= XK_F1 as u64 && ksym <= XK_F35 as u64 => Ok(Key::FUNCTION),
+        _ if ksym >= 0x11000000 && ksym <= 0x1100FFFF => Ok(Key::PRIVATEKEYPAD),
+        _ if ksym >= XK_Select as u64 && ksym <= XK_Break as u64 => Ok(Key::MISCFUNCTION),
+        _ if ksym >= XK_F1 as u64 && ksym <= XK_F4 as u64 => Ok(Key::PF),
+        _ => Err("Not an extra key or not a valid key")
     }
 }
